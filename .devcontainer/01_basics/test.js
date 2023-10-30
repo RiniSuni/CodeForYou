79b5d6fd122529682f20dd6310e7d180bbb1652e
RiniSuni <149391856+RiniSuni@users.noreply.github.com>
2023-10-30 13:56:28 +0000

    Array Done

---
+++ .devcontainer/01_basics/test.js
@@ -0,0 +1,17 @@
+// const myArr = [1,2,3,4,5,6]
+
+// console.log(myArr.slice(2,4))
+// console.log(myArr.splice(2,4))
+
+// const myArr2 = [1,2,4,34,4.5,[3,"djikjg",7],[2,"rfa",[2,8,0,5]]]
+
+// console.log(myArr2.flat(Infinity))
+
+
+console.log(Array.isArray([1,2,3,4]))
+console.log(Array.from("Sunil Kolle"))
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
